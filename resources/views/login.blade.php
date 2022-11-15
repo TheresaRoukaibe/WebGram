7faa90bc4a36a982cd49f22470dbe03911ca0955
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-15 14:24:21 +0200

    Adding stylesheet

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,9 +10,26 @@
     </head>
     <body>
        <div class= "container">
+
         <div class = "logo">
         <img class="img" src=" {{asset('imgs/webgram-logo.png') }}">
 </div>
+
+<div class = "login">
+   <h1 class="title"> SIGN IN <h1>  
+    <h1 class = "line"> </h1>
+    <input class ="input" type="text" placeholder="Email..." name="email" required>
+    <input class ="input" type="password" placeholder="Password..." name="pass" required>
+    <div style="text-align:center">
+    <button class="button" type="button">SUBMIT</button>
+</div>
+
+<div style="text-align:center">
+    <h1 class="separator" > </h1>
+    <h3 class="signup">Don't have an account? <span class="link">Click Me </span> to sign up! </h3>
+</div> 
+</div>
+
 </div> 
 
         
