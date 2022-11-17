9f722a2492596fc37a15fae39066de4575569883
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-17 13:39:34 +0200

    Designing add post

---
+++ resources/views/add_post.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload New Pic</title>
+    <link rel="stylesheet" href="css/add.css">
+</head>
+<body>
+<div class="container">
+  <form class="form">
+    <div class="file-upload-wrapper" data-text="Select your file!">
+      <input name="file-upload-field" type="file" class="file-upload-field" value="">
+      <div>
+      <label for="desc"  class="file-upload-field">Describe your picture</label>
+      
+</div>
+    </div>
+    <input type="text" class="file-upload-field" name="desc" id="desc" />
+    <div class="form-group">
+                            <input type="submit" name="submit" id="submit" class="file-upload-field" value="POST"/>
+                        </div>
+  </form>
+</div>
+</body>
+</html>
