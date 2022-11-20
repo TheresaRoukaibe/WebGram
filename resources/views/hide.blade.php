6f70cce2667d126661a796058837bf7845f4759b
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-20 18:00:42 +0200

    Making hide design

--- resources/views/hide.blade.php
+++ resources/views/hide.blade.php
@@ -5,8 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hide Your Post</title>
+    <link rel="stylesheet" href="{{ asset('css/hide.css') }}">
 </head>
 <body>
+<div class="container">
+  <form class="form" method="GET" action="{{'hide'}}" >
+    @csrf
+      <div>
+      <label for="desc"  class="file-upload-field">Enter the email of the account you wish to hide this from</label>
+      <input type="text" class="file-upload-field" name="email" id="email" />
     
+</div>
+    
+<button type="submit"  class="button" type="button">HIDE</button>
+  </form>
+
 </body>
 </html>
